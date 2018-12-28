43f9326a7e1b2c6c0f48963da16f59b8196beaae
Uros Andjelic <uros.andjelic@diwanee.com>
2018-12-28 14:09:06 +0100

    vs code setup init

---
+++ vs-code/extensions.sh
@@ -0,0 +1,7 @@
+code --install-extension bmewburn.vscode-intelephense-client
+code --install-extension eamodio.gitlens
+code --install-extension Equinusocio.vsc-material-theme
+code --install-extension marabesi.php-import-checker
+code --install-extension ms-mssql.mssql
+code --install-extension ms-python.python
+code --install-extension sleistner.vscode-fileutils
